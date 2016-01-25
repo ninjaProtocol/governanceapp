2e095b7b8e96a6c9d7e0f7f9252d0e9573b22b15
ninjaProtocol <ojassarup@hotmail.com>
2016-01-25 17:53:42 +0530

    Splash screen, login screen, user screen, authenticator

---
+++ app/src/main/java/com/example/ojas/governanceapp/AuthenticateUser.java
@@ -0,0 +1,47 @@
+package com.example.ojas.governanceapp;
+
+/**
+ * Created by Ojas on 22-01-2016.
+ */
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class AuthenticateUser {
+
+    private short ERRORLEVEL;
+
+    AuthenticateUser(String plainTextEmail, String plainTextPassword) {
+
+        //authentication should be server side
+        String mock_userEmail = "admin@iora.com";
+        //String mock_userPassword = "5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8"; // SHA1 of "password"
+        String mock_userPassword = "password";
+
+        //calculate checksum and transmit to server for auth
+        //String passwordHash = DigestUtils.sha1Hex(plainTextPassword);
+
+        //send checksum to server for validation and set ERRORLEVEL
+        if (plainTextEmail.equalsIgnoreCase(mock_userEmail)) {
+            if (mock_userPassword.equalsIgnoreCase(plainTextPassword)) {
+                ERRORLEVEL = 0; //successful match
+            }
+            else
+                ERRORLEVEL = 1; //password doesn't match
+        } else
+            ERRORLEVEL = 3; //email doesn't match
+    }
+
+    public short getErrorLevel(){
+
+        /*String passwordHash = DigestUtils.sha1Hex(plainTextPassword); //calculate checksum
+
+        //send checksum to server for validation
+        if (plainTextEmail.matches(mock_userEmail)) {
+            if (mock_userPassword.equalsIgnoreCase(passwordHash))
+                ERRORLEVEL = 0; //successful match
+            else
+                ERRORLEVEL = 1; //password doesn't match
+        }*/
+        return ERRORLEVEL;
+    }
+}
---
+++ app/src/main/java/com/example/ojas/governanceapp/LoginActivity.java
@@ -0,0 +1,44 @@
+package com.example.ojas.governanceapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void onClickMethod (View view) {
+
+        final EditText getEmailText = (EditText) findViewById(R.id.id_editText_email);
+        String emailText = getEmailText.getText().toString();
+        final EditText getPasswordText = (EditText) findViewById(R.id.id_editText_password);
+        String passwordText = getPasswordText.getText().toString();
+
+        AuthenticateUser authenticatorObject = new AuthenticateUser(emailText,passwordText);
+
+        switch (authenticatorObject.getErrorLevel()) {
+
+            case 0: Intent I = new Intent(this,WelcomeScreen.class); //proceed to user screen
+                    startActivity(I);
+                    break;
+
+            case 1: //message - wrong password
+                    break;
+
+            case 2: //message - wrong email
+                    break;
+
+            default: //message - exception, do nothing
+        }
+    }
+}
