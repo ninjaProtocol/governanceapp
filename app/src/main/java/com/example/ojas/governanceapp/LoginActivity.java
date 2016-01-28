5557cb147bcfa8faf4dbadbc2bc6e520a64273af
ninjaProtocol <ojassarup@hotmail.com>
2016-01-28 17:50:03 +0530

    Smarter splash screen, added preference file, added logging, toasts in login screen.

--- app/src/main/java/com/example/ojas/governanceapp/LoginActivity.java
+++ app/src/main/java/com/example/ojas/governanceapp/LoginActivity.java
@@ -1,11 +1,14 @@
 package com.example.ojas.governanceapp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.content.Intent;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -19,26 +22,38 @@ public class LoginActivity extends AppCompatActivity {
 
     public void onClickMethod (View view) {
 
-        final EditText getEmailText = (EditText) findViewById(R.id.id_editText_email);
+        Context context = getApplicationContext();
+        Toast toast;
+
+        final EditText getEmailText = (EditText) findViewById(R.id.editText_email);
         String emailText = getEmailText.getText().toString();
-        final EditText getPasswordText = (EditText) findViewById(R.id.id_editText_password);
+        final EditText getPasswordText = (EditText) findViewById(R.id.editText_password);
         String passwordText = getPasswordText.getText().toString();
 
         AuthenticateUser authenticatorObject = new AuthenticateUser(emailText,passwordText);
 
         switch (authenticatorObject.getErrorLevel()) {
 
-            case 0: Intent I = new Intent(this,WelcomeActivity.class); //proceed to user screen
-                    startActivity(I);
-                    break;
+            case 0:
+                Intent I = new Intent(this, WelcomeActivity.class); //proceed to user screen
+                startActivity(I);
+                finish();
+                break;
 
             case 1: //message - wrong password
-                    break;
+                Log.d("TAG__AUTH_EXCEPTION", "wrong password");
+                toast = Toast.makeText(context,"Wrong Password",Toast.LENGTH_LONG);
+                toast.show();
+                break;
 
             case 2: //message - wrong email
-                    break;
+                Log.d("TAG_AUTH_MESSAGE", "wrong email ID");
+                toast = Toast.makeText(context,"Wrong Email ID",Toast.LENGTH_LONG);
+                toast.show();
+                break;
 
             default: //message - exception, do nothing
+                Log.d("TAG_AUTH_EXCEPTION", "Authenticator threw an exception");
         }
     }
 }
--- app/src/main/java/com/example/ojas/governanceapp/SplashActivity.java
+++ app/src/main/java/com/example/ojas/governanceapp/SplashActivity.java
@@ -1,15 +1,89 @@
 package com.example.ojas.governanceapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this,LoginActivity.class);
+
+        //get handle to shared pref file to activity
+        SharedPreferences sharedPreferences = this.getSharedPreferences(getString(R.string.prefFile_login),MODE_PRIVATE);
+
+        //check if pref file contains "saveData" key, if yes then this isn't the first run of app
+        //check in pref file whether user wants to be remembered (i.e. saveData is true)
+        //check in pref file whether user is logged in
+        if (sharedPreferences.contains(getString(R.string.prefFile_key_saveData)))
+        {
+            if (sharedPreferences.getBoolean(getString(R.string.prefFile_key_isUserLoggedIn),false)) {
+                AuthenticateUser authenticatorObject = new AuthenticateUser(
+                        sharedPreferences.getString(getString(R.string.prefFile_key_currentUser), ""),
+                        sharedPreferences.getString(getString(R.string.prefFile_key_currentPassword), "")
+                );
+
+                switch (authenticatorObject.getErrorLevel()) {
+
+                    case 0:
+                        Intent I = new Intent(this, WelcomeActivity.class); //proceed to user screen
+                        startActivity(I);
+                        finish();
+                        break;
+
+                    case 1: //message - wrong password
+                        Log.d("TAG_AUTH_EXCEPTION", "wrong password");
+                        break;
+
+                    case 2: //message - wrong email
+                        Log.d("TAG_AUTH_MESSAGE", "wrong email ID");
+                        break;
+
+                    default: //message - exception, do nothing
+                        Log.d("TAG_AUTH_EXCEPTION", "Authenticator threw an exception");
+                }
+            } else {
+                if (sharedPreferences.getBoolean(getString(R.string.prefFile_key_saveData), false)){
+                    Log.d("TAG_SPLASH_MESSAGE","User data saved but user not logged in");
+                    //proceed to login screen
+                    Intent intent = new Intent(this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        } else{
+            Log.d("TAG_SPLASH_MESSAGE","PrefFile likely doesn't exist");
+
+            //init pref file
+            Log.d("TAG_SPLASH_MESSAGE","Initialising sharedPreferences file");
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean(getString(R.string.prefFile_key_saveData), false);
+            editor.putString(getString(R.string.prefFile_key_currentUser), "");
+            editor.putString(getString(R.string.prefFile_key_currentPassword), "");
+            editor.putBoolean(getString(R.string.prefFile_key_isUserLoggedIn),false);
+            if (editor.commit()) {
+                Log.d("TAG_FILE_MESSAGE","Successfully wrote to pref file");
+            } else {
+                Log.d("TAG_FILE_ERROR","Could not commit to pref file");
+            }
+        }
+
+        //if you're here then that means that it's likely that the user didn't save data.
+        Log.d("TAG_SPLASH_MESSAGE","User hasn't saved data, possible new user");
+
+        //hold up for 3 seconds so that user sees splash
+        try {
+            synchronized (this) {
+                wait(3000);
+            }
+        } catch (Exception e){
+            Log.d("TAG_SPLASH_EXCEPTION","wait() threw an exception");
+        }
+
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         finish();
     }
